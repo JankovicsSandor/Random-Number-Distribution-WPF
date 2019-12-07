060c82fa5fe2dc411d8729cd786eb34ddde38649
Jankovics Sándor <jankovics.sandor.f@gmail.com>
2019-12-07 11:03:43 +0100

    Add gitignore

---
+++ RandomDistributionGenerator/Model/GeneratedNumberStat.cs
@@ -0,0 +1,27 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomDistributionGenerator.Model
+{
+    class GeneratedNumberStat : ObservableObject
+    {
+        private int number;
+        private int numberOfChoice;
+
+        public int NumberOfChoice
+        {
+            get { return numberOfChoice; }
+            set { Set(ref numberOfChoice, value); }
+        }
+
+
+        public int Number
+        {
+            get { return number; }
+            set { Set(ref number, value); }
+        }
+
+    }
+}
---
+++ RandomDistributionGenerator/VM/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomDistributionGenerator.VM
+{
+    class MainWindowViewModel:ViewModelBase
+    {
+    }
+}
